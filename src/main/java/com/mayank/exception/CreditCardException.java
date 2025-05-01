ae48a6971694eb6f423df99b2dd93343ea37f25f
obaidrehman30 <obaidrehman3009@gmail.com>
2025-05-02 02:28:05 +0530

    Update CreditCardException

--- src/main/java/com/mayank/exception/CreditCardException.java
+++ src/main/java/com/mayank/exception/CreditCardException.java
@@ -1,13 +1,1 @@
-package com.mayank.exception;
 
-public class CreditCardException extends Exception {
-
-	public CreditCardException() {
-
-	}
-
-	public CreditCardException(String msg) {
-		super(msg);
-	}
-
-}
